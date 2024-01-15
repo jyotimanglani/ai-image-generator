912b5231e9c4204d27dad1148a0b61d76d2efcea
Jyoti Manglani <manglanijyoti62@gmail.com>
2024-01-15 18:40:43 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ImageGenerator from "./components/imageGenerator/ImageGenerator";
+
+function App() {
+  return (
+    <div>
+      <ImageGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/imageGenerator/ImageGenerator.jsx
@@ -0,0 +1,9 @@
+import "./ImageGenerator.css"
+
+const ImageGenerator = () => {
+  return (
+    <div>ImageGenerator</div>
+  )
+}
+
+export default ImageGenerator
